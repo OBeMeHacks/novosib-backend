2b8b4d235bd3423de6436be2c3ce7d12a8236964
Step2Victory <stepanmalov555@gmail.com>
2023-09-30 17:45:41 +0000

    add

--- model.py
+++ model.py
@@ -5,32 +5,30 @@ from catboost import CatBoostRegressor
 from .timeseries_train import train_models, predict 
 
 
-def preprocess(contrib_cleaned):
-    contrib_cleaned['accnt_pnsn_schm'] = contrib_cleaned.accnt_pnsn_schm.astype(float).fillna(
-        contrib_cleaned.accnt_pnsn_schm.mode()[0]
+def preprocess_contributors(contributors):
+    contributors['accnt_pnsn_schm'] = contributors.accnt_pnsn_schm.astype(float).fillna(
+        contributors.accnt_pnsn_schm.mode()[0]
         ).astype(int).astype('category')
-    blnc_filter = (((contrib_cleaned.npo_blnc.isna()) | (contrib_cleaned.npo_blnc < 0)) & (contrib_cleaned.npo_accnt_status == 1))
-    contrib_cleaned.loc[blnc_filter, 'npo_blnc'] = contrib_cleaned.loc[blnc_filter, 'npo_pmnts_sum'].fillna(0)
-    contrib_cleaned.loc[contrib_cleaned.npo_accnt_status == 0, 'npo_blnc'] = np.nan
-    contrib_cleaned.loc[contrib_cleaned.npo_pmnts_sum < 0, 'npo_pmnts_sum'] = np.nan
-    contrib_cleaned.loc[contrib_cleaned.npo_ttl_incm < 0, 'npo_ttl_incm'] = 0
-    contrib_cleaned['npo_ttl_incm'] = contrib_cleaned.npo_ttl_incm.fillna(0)
-    contrib_cleaned.dropna(subset=['npo_pmnts_sum'], inplace=True)
-    contrib_cleaned.reset_index(drop=True, inplace=True)
-    return contrib_cleaned
+    blnc_filter = (((contributors.npo_blnc.isna()) | (contributors.npo_blnc < 0)) & (contributors.npo_accnt_status == 1))
+    contributors.loc[blnc_filter, 'npo_blnc'] = contributors.loc[blnc_filter, 'npo_pmnts_sum'].fillna(0)
+    contributors.loc[contributors.npo_accnt_status == 0, 'npo_blnc'] = np.nan
+    contributors.loc[contributors.npo_pmnts_sum < 0, 'npo_pmnts_sum'] = np.nan
+    contributors.loc[contributors.npo_ttl_incm < 0, 'npo_ttl_incm'] = 0
+    contributors['npo_ttl_incm'] = contributors.npo_ttl_incm.fillna(0)
+    contributors.dropna(subset=['npo_pmnts_sum'], inplace=True)
+    contributors.reset_index(drop=True, inplace=True)
+    return contributors
 
 
-def extract_features_table_from_contributors(path_to_data):
-    data = pd.read_csv(path_to_data)
+def extract_features_table_from_contributors(contributors):
+    processed_contributors = contributors.copy()
 
-    contributors = preprocess(data)
+    processed_contributors['date'] =  pd.to_datetime(processed_contributors['npo_accnt_status_date']).dt.to_period('Q')
+    processed_contributors['npo_accnt_status_date'] = pd.to_datetime(processed_contributors['npo_accnt_status_date'])
+    processed_contributors['npo_lst_pmnt_date'] = pd.to_datetime(processed_contributors['npo_lst_pmnt_date'])
+    processed_contributors['npo_frst_pmnt_date'] = pd.to_datetime(processed_contributors['npo_frst_pmnt_date'])
 
-    contributors['date'] =  pd.to_datetime(contributors['npo_accnt_status_date']).dt.to_period('Q')
-    contributors['npo_accnt_status_date'] = pd.to_datetime(contributors['npo_accnt_status_date'])
-    contributors['npo_lst_pmnt_date'] = pd.to_datetime(contributors['npo_lst_pmnt_date'])
-    contributors['npo_frst_pmnt_date'] = pd.to_datetime(contributors['npo_frst_pmnt_date'])
-
-    features = contributors.groupby(["date"]).agg(
+    features = processed_contributors.groupby(["date"]).agg(
         {
             'npo_accnt_id' : 'nunique',
             'npo_accnt_status_date' : ['min', 'max'],
@@ -50,54 +48,89 @@ def extract_features_table_from_contributors(path_to_data):
     return features.sort_values(by='date', ascending=False)
 
 
-def get_target(path_to_target):
-    target = pd.read_csv(path_to_target)
-    return target
+def get_target(contributors, transactions):
+    target = transactions.copy()
+    mapper = contributors[['npo_accnt_id', 'clnt_id']]
+    target = target.merge(mapper).drop(['npo_accnt_id'], axis=1)
+    target.npo_operation_date = pd.to_datetime(target.npo_operation_date)
+    target = target[(target.npo_sum > 0) & (target.npo_operation_group == 0)]
+    target = target.groupby(['clnt_id', target['date'].dt.to_period('Q')]).agg(
+        {'slctn_nmbr' : 'count', 'npo_sum':'sum'}).reset_index().rename(columns={'slctn_nmbr':'count_contributions', 
+                                                                                 'npo_sum':'SUM'})
+    target['mean_contribution'] = target['SUM'] / target['count_contributions']
 
-class ClientFeatureExtractor:
+    return target.drop(['SUM'], axis=1)
+
+
+def prepare_dates(contribution, transaction):
+    pass
+
+
+class FeatureExtractor:
     def __init__(self, path_to_data):
         self.data = extract_features_table_from_contributors(path_to_data)
-        self.data_by_user = {
-            client_id: self.extract_user_table_(data, client_id) 
-            for client_id in self.all_client_ids
-            }
         self.features_next_quarter = dict()
-        self.feature_columns = df.columns
-        for client_id, df in self.data.items():
-            result = dict()
-            for column in df.columns:
-                ts = df[column]
-                result[column] = predict(ts, train_models(ts)[0])[0]
-            self.features_next_quarter[client_id] = result
-
-
-    def extract_user_table_(self, data, client_id):
-        return data[data['clnt_id'] == client_id].drop(columns=['clnt_id'], axis=1)
+        self.feature_columns = self.data.columns
+        result = dict()
+        for column in self.feature_columns:
+            ts = self.data[column]
+            result[column] = predict(ts, train_models(ts)[0])[0]
+        self.features_next_quarter = result
     
-    def get_features_values(self, client_id):
-        return self.features_next_quarter[client_id]
+    def get_features_values_next_quarter(self):
+        return self.features_next_quarter
+    
+    def get_feature_values(self, quarter):
+        return self.get_feature_values[quarter]
+    
+    def get_features(self):
+        return self.get_feature_values
 
 
 class Model:
 
     def __init__(self,
                  path_to_external_data,
-                 path_to_transactions_table,
-                 path_to_contributors_table):
-        self.client_feature_extractor = ClientFeatureExtractor(path_to_contributors_table)
-        self.data_train = pd.read_csv(path_to_external_data)
-        self.target = get_target(path_to_transactions_table)
+                 path_to_clients_table,
+                 path_to_contributors_table,
+                 path_to_transactions_table
+                 ):
+        # self.data_train = pd.read_csv(path_to_external_data)
+        transactions = pd.read_csv(path_to_transactions_table)
+        contributors = preprocess_contributors(pd.read_csv(path_to_contributors_table))
+        clients = pd.read_csv(path_to_clients_table)
+
+        self.client_feature_extractor = FeatureExtractor(contributors)
+        self.internal_features = self.client_feature_extractor.get_features()
+        self.target = get_target(contributors, transactions)
+        X_train, y_train = self.get_train_data(clients)
         self.model = CatBoostRegressor().fit(
-            self.data_train,
-            self.target,
+            X_train,
+            y_train,
         )
 
+    def get_train_data(self, clients):
+        merged = pd.merge(self.target, self.internal_features, on='date')
+        all_merged = pd.merge(merged, clients, on='clnt_id')
 
-    
+        y_train = all_merged[all_merged['date'] != '2022Q2']['mean_contribution']
+        X_train = all_merged[all_merged['date'] != '2022Q2'].drop(
+            [
+                'mean_contribution',
+                'count_contributions',
+                'min_payment_date',
+                'max_payment_date',
+                'date',
+                'pstl_code'
+            ], axis=1)
+        
+        return X_train, y_train
+
+
 
 
     def predict(self, client_id, override_features):
-        features = self.client_feature_extractor.get_features_values[client_id]
+        features = self.client_feature_extractor.get_features_values()
         features.update(override_features)
         return self.model.predict(features)
 
