85e11a169d37e43097dd83d4beace0c6ec2cc903
Step2Victory <stepanmalov555@gmail.com>
2023-09-30 14:57:46 +0000

    add

--- model.py
+++ model.py
@@ -1,140 +1,57 @@
 import numpy as np
 import pandas as pd
-from catboost import Pool, CatBoostRegressor
-import datetime
-import typing as tp
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
+from catboost import CatBoostRegressor
+
+from .timeseries_train import train_models, predict 
 
 
+def preprocess(contrib_cleaned):
+    contrib_cleaned['accnt_pnsn_schm'] = contrib_cleaned.accnt_pnsn_schm.astype(float).fillna(
+        contrib_cleaned.accnt_pnsn_schm.mode()[0]
+        ).astype(int).astype('category')
+    blnc_filter = (((contrib_cleaned.npo_blnc.isna()) | (contrib_cleaned.npo_blnc < 0)) & (contrib_cleaned.npo_accnt_status == 1))
+    contrib_cleaned.loc[blnc_filter, 'npo_blnc'] = contrib_cleaned.loc[blnc_filter, 'npo_pmnts_sum'].fillna(0)
+    contrib_cleaned.loc[contrib_cleaned.npo_accnt_status == 0, 'npo_blnc'] = np.nan
+    contrib_cleaned.loc[contrib_cleaned.npo_pmnts_sum < 0, 'npo_pmnts_sum'] = np.nan
+    contrib_cleaned.loc[contrib_cleaned.npo_ttl_incm < 0, 'npo_ttl_incm'] = 0
+    contrib_cleaned['npo_ttl_incm'] = contrib_cleaned.npo_ttl_incm.fillna(0)
+    contrib_cleaned.dropna(subset=['npo_pmnts_sum'], inplace=True)
+    contrib_cleaned.reset_index(drop=True, inplace=True)
+    return contrib_cleaned
 
-X_type = tp.NewType("X_type", np.ndarray)
-X_row_type = tp.NewType("X_row_type", np.ndarray)
-Y_type = tp.NewType("Y_type", np.array)
-TS_type = tp.NewType("TS_type", pd.Series)
-Model_type = tp.TypeVar("Model_type")
 
+def extract_features(path_to_data):
+    data = pd.read_csv(path_to_data)
+    contributors = preprocess(data)
 
-
-
-def extract_hybrid_strategy_features(
-    timeseries: TS_type,
-    model_idx: int,
-    window_size: int = 7
-) -> X_row_type:
-    """
-    Функция для получения вектора фичей согласно гибридной схеме. На вход подаётся временной ряд
-    до момента T, функция выделяет из него фичи, необходимые модели под номером model_idx для
-    прогноза на момент времени T
+    contributors['date'] =  pd.to_datetime(contributors['npo_accnt_status_date']).dt.to_period('Q')
+    contributors['npo_accnt_status_date'] = pd.to_datetime(contributors['npo_accnt_status_date'])
+    contributors['npo_lst_pmnt_date'] = pd.to_datetime(contributors['npo_lst_pmnt_date'])
+    contributors['npo_frst_pmnt_date'] = pd.to_datetime(contributors['npo_frst_pmnt_date'])
     
-    Args:
-        timeseries --- временной ряд до момента времени T (не включительно), pd.Series с датой 
-                       в качестве индекса
-        model_idx --- индекс модели, то есть номер шага прогноза, 
-                      для которого нужно получить признаки, нумерация с нуля
-        window_size --- количество последних значений ряда, используемых для прогноза 
-                        (без учёта количества прогнозов с предыдущих этапов)
-
-    Returns:
-        Одномерный вектор фичей для модели с индексом model_idx (np.array), 
-        чтобы сделать прогноз для момента времени T
-    """
-    feature_window = window_size + model_idx # YOUR CODE HERE
-    return timeseries[-feature_window:].values
+    features_for_ts = contributors.groupby(["clnt_id", "date"]).agg(
+        {
+            'npo_accnt_id' : 'nunique',
+            'npo_accnt_status_date' : 'max',
+            'npo_pmnts_sum' : 'sum',
+            'npo_pmnts_nmbr' : 'sum',
+            'npo_frst_pmnt_date' : 'min',
+            'npo_lst_pmnt_date' : 'max',
+            'npo_ttl_incm' : 'sum',
+        }
+    )
+    return features_for_ts
 
 
-def build_datasets(
-    timeseries: TS_type,
-    extract_features: tp.Callable[..., X_row_type],
-    window_size: int,
-    model_count: int
-) -> tp.List[tp.Tuple[X_type, Y_type]]:
-    """
-    Функция для получения обучающих датасетов согласно гибридной схеме
-    
-    Args:
-        timeseries --- временной ряд
-        extract_features --- функция для генерации вектора фичей
-        window_size --- количество последних значений ряда, используемых для прогноза
-        model_count --- количество моделей, используемых для получения предскзаний 
-
-    Returns:
-        Список из model_count датасетов, i-й датасет используется для обучения i-й модели 
-        и представляет собой пару из двумерного массива фичей и одномерного массива таргетов
-    """
-    datasets = []
-    for i in range(model_count):
-        datasets.append([[], []])
-    # YOUR CODE HERE
-    for t in range(window_size, len(timeseries)):
-        for model_idx in range(min(t - window_size + 1, model_count)):
-            datasets[model_idx][0].append(extract_features(timeseries[:t], model_idx, window_size))
-            datasets[model_idx][1].append(timeseries[t])
-    assert len(datasets) == model_count
-    return datasets
-
-
-def train_models(
-        train_timeseries: TS_type,
-        model_count: int
-    ) -> tp.List[Model_type]:
-        """
-        Функция для получения обученных моделей
-        
-        Args:
-            train_timeseries --- обучающий временной ряд
-            model_count --- количество моделей для обучения согласно гибридной схеме.
-                            Прогнозирование должно выполняться на model_count дней вперёд
-
-        Returns:
-            Список из len(datasets) обученных моделей
-        """
-        models = []
-
-        datasets = build_datasets(train_timeseries, extract_hybrid_strategy_features, 7, model_count)
-        for train, target in datasets:
-            models.append(GradientBoostingRegressor().fit(train, target))
-        
-        assert len(models) == len(datasets)
-        return models
-
-
-def predict(
-    timeseries: TS_type,
-    models: tp.List[Model_type],
-    extract_features: tp.Callable[..., X_row_type] = extract_hybrid_strategy_features
-) -> TS_type:
-    """
-    Функция для получения прогноза len(models) следующих значений временного ряда
-    
-    Args:
-        timeseries --- временной ряд, по которому необходимо сделать прогноз на следующие даты
-        models --- список обученных моделей, i-я модель используется для получения i-го прогноза
-        extract_features --- функция для генерации вектора фичей. Если вы реализуете свою функцию 
-                             извлечения фичей для конечной модели, передавайте этим аргументом.
-                             Внутри функции predict функцию extract_features нужно вызывать только
-                             с аргументами timeseries и model_idx, остальные должны быть со значениями
-                             по умолчанию
-
-    Returns:
-        Прогноз len(models) следующих значений временного ряда
-    """
-    prediction = []
-    for i, model in enumerate(models):
-        features = extract_features(timeseries, i)
-        prediction.append(model.predict([features]))
-        pred = pd.Series(prediction[-1], index = [timeseries.index[-1] +  (timeseries.index[-1] - timeseries.index[-2])])
-        timeseries = pd.concat([timeseries, pred], ignore_index=False)
-    return timeseries[-len(models):]
-
-
+def get_target(path_to_target):
+    target = pd.read_csv(path_to_target)
+    return target
 
 class ClientFeatureExtractor:
     def __init__(self, path_to_data):
-        self.all_client_ids = open(path_to_data + '/client_ids').read().split()
-        self.data = {
-            client_id: self.extract_user_table_(path_to_data, client_id) 
+        self.data = extract_features_table_from_contributors(path_to_data)
+        self.data_by_user = {
+            client_id: self.extract_user_table_(data, client_id) 
             for client_id in self.all_client_ids
             }
         self.features_next_quarter = dict()
@@ -147,8 +64,8 @@ class ClientFeatureExtractor:
             self.features_next_quarter[client_id] = result
 
 
-    def extract_user_table_(self, path, client_id):
-        return pd.read_csv(path + '/' + client_id + '.csv')
+    def extract_user_table_(self, data, client_id):
+        data[]
     
     def get_features_values(self, client_id):
         return self.features_next_quarter[client_id]
@@ -158,15 +75,19 @@ class Model:
 
     def __init__(self,
                  path_to_external_data,
-                 path_to_target,
-                 path_to_internal_data):
-        self.client_feature_extractor = ClientFeatureExtractor(path_to_internal_data)
+                 path_to_transactions_table,
+                 path_to_contributors_table):
+        self.client_feature_extractor = ClientFeatureExtractor(path_to_contributors_table)
         self.data_train = pd.read_csv(path_to_external_data)
-        self.target = pd.read_csv(path_to_target)
+        self.target = get_target(path_to_transactions_table)
         self.model = CatBoostRegressor().fit(
             self.data_train,
             self.target,
         )
+
+
+    
+
 
     def predict(self, client_id, override_features):
         features = self.client_feature_extractor.get_features_values[client_id]
---
+++ timeseries_train.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import typing as tp
+from sklearn.ensemble import GradientBoostingRegressor
+
+
+X_type = tp.NewType("X_type", np.ndarray)
+X_row_type = tp.NewType("X_row_type", np.ndarray)
+Y_type = tp.NewType("Y_type", np.array)
+TS_type = tp.NewType("TS_type", pd.Series)
+Model_type = tp.TypeVar("Model_type")
+
+
+
+
+def extract_hybrid_strategy_features(
+    timeseries: TS_type,
+    model_idx: int,
+    window_size: int = 7
+) -> X_row_type:
+    """
+    Функция для получения вектора фичей согласно гибридной схеме. На вход подаётся временной ряд
+    до момента T, функция выделяет из него фичи, необходимые модели под номером model_idx для
+    прогноза на момент времени T
+    
+    Args:
+        timeseries --- временной ряд до момента времени T (не включительно), pd.Series с датой 
+                       в качестве индекса
+        model_idx --- индекс модели, то есть номер шага прогноза, 
+                      для которого нужно получить признаки, нумерация с нуля
+        window_size --- количество последних значений ряда, используемых для прогноза 
+                        (без учёта количества прогнозов с предыдущих этапов)
+
+    Returns:
+        Одномерный вектор фичей для модели с индексом model_idx (np.array), 
+        чтобы сделать прогноз для момента времени T
+    """
+    feature_window = window_size + model_idx # YOUR CODE HERE
+    return timeseries[-feature_window:].values
+
+
+def build_datasets(
+    timeseries: TS_type,
+    extract_features: tp.Callable[..., X_row_type],
+    window_size: int,
+    model_count: int
+) -> tp.List[tp.Tuple[X_type, Y_type]]:
+    """
+    Функция для получения обучающих датасетов согласно гибридной схеме
+    
+    Args:
+        timeseries --- временной ряд
+        extract_features --- функция для генерации вектора фичей
+        window_size --- количество последних значений ряда, используемых для прогноза
+        model_count --- количество моделей, используемых для получения предскзаний 
+
+    Returns:
+        Список из model_count датасетов, i-й датасет используется для обучения i-й модели 
+        и представляет собой пару из двумерного массива фичей и одномерного массива таргетов
+    """
+    datasets = []
+    for i in range(model_count):
+        datasets.append([[], []])
+    # YOUR CODE HERE
+    for t in range(window_size, len(timeseries)):
+        for model_idx in range(min(t - window_size + 1, model_count)):
+            datasets[model_idx][0].append(extract_features(timeseries[:t], model_idx, window_size))
+            datasets[model_idx][1].append(timeseries[t])
+    assert len(datasets) == model_count
+    return datasets
+
+
+def train_models(
+        train_timeseries: TS_type,
+        model_count: int
+    ) -> tp.List[Model_type]:
+        """
+        Функция для получения обученных моделей
+        
+        Args:
+            train_timeseries --- обучающий временной ряд
+            model_count --- количество моделей для обучения согласно гибридной схеме.
+                            Прогнозирование должно выполняться на model_count дней вперёд
+
+        Returns:
+            Список из len(datasets) обученных моделей
+        """
+        models = []
+
+        datasets = build_datasets(train_timeseries, extract_hybrid_strategy_features, 7, model_count)
+        for train, target in datasets:
+            models.append(GradientBoostingRegressor().fit(train, target))
+        
+        assert len(models) == len(datasets)
+        return models
+
+
+def predict(
+    timeseries: TS_type,
+    models: tp.List[Model_type],
+    extract_features: tp.Callable[..., X_row_type] = extract_hybrid_strategy_features
+) -> TS_type:
+    """
+    Функция для получения прогноза len(models) следующих значений временного ряда
+    
+    Args:
+        timeseries --- временной ряд, по которому необходимо сделать прогноз на следующие даты
+        models --- список обученных моделей, i-я модель используется для получения i-го прогноза
+        extract_features --- функция для генерации вектора фичей. Если вы реализуете свою функцию 
+                             извлечения фичей для конечной модели, передавайте этим аргументом.
+                             Внутри функции predict функцию extract_features нужно вызывать только
+                             с аргументами timeseries и model_idx, остальные должны быть со значениями
+                             по умолчанию
+
+    Returns:
+        Прогноз len(models) следующих значений временного ряда
+    """
+    prediction = []
+    for i, model in enumerate(models):
+        features = extract_features(timeseries, i)
+        prediction.append(model.predict([features]))
+        pred = pd.Series(prediction[-1], index = [timeseries.index[-1] +  (timeseries.index[-1] - timeseries.index[-2])])
+        timeseries = pd.concat([timeseries, pred], ignore_index=False)
+    return timeseries[-len(models):]
